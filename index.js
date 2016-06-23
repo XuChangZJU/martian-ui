4d7a32701aeb19bf05865ff8188f853371a68041
babyLiyuan <18209223080@163.com>
2016-06-23 09:34:38 +0800

    fix a conflict

--- index.js
+++ index.js
@@ -5,16 +5,10 @@
 var MartianUI = {
     // Components
     MtSeparator:require('./lib/Separator'),
-<<<<<<< HEAD
     MtText:require('./lib/Text'),
     MtCaptcha:require('./lib/Captcha'),
     MtContainer:require('./lib/Container'),
     MtNavBar:require('./lib/NavBar')
-=======
-    MtChar:require('./lib/Text'),
-    MtCaptcha:require('./lib/Captcha')
->>>>>>> 23d6dd68126d61cdb7153fd0fe696ad0e2544133
-
 };
 
 
