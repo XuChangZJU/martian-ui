7578f11ccbb10b659f1d173b5613b0fdbc685690
lavystord <lavystord@hotmail.com>
2016-07-04 14:10:44 +0800

    refactor MtNavBar

--- Example/MtUI/lib/NavBarExample.js
+++ Example/MtUI/lib/NavBarExample.js
@@ -1,5 +1,5 @@
 /**
- * Created by Bly on 2016/6/13.
+ * Created by Hey on 2016/7/04.
  */
 'use strict';
 
@@ -19,16 +19,6 @@ exports.title  = 'MtNavBar';
 exports.description = 'NavBar is navigation bar.';
 exports.examples = [
     {
-        title: 'Plain NavBar',
-        render(): ReactElement {
-            return (
-                <MtNavBar pop = {()=> alert("hello")}
-                />
-
-            );
-        },
-    },
-    {
         title: 'NavBar with title',
         render(): ReactElement {
             return (
@@ -38,6 +28,90 @@ exports.examples = [
 
             );
         },
+    },
+  {
+    title:'NavBar with left backIcon',
+    render():ReactElement {
+      let leftItem = {
+        // title: 'left',
+        icon: require('./img/back_white.png'),
+        onPress:() => {alert('go back')}
+      };
+      return (
+        <MtNavBar
+          title = 'hello world'
+          leftItem={leftItem}
+        >
+        </MtNavBar>
+      );
     }
+  },
+  {
+    title:'NavBar with left Icon and right title',
+    render():ReactElement {
+      let leftItem = {
+        icon: require('./img/back_white.png'),
+        onPress:() => {alert('go back')},
+      };
+      let rightItem = {
+        // icon: require('./img/settings_white.png'),
+        title:'right',
+        onPress:() => {alert('setting')}
+      };
+      return (
+        <MtNavBar
+          title = 'hello world'
+          leftItem={leftItem}
+          rightItem = {rightItem}
+        >
+        </MtNavBar>
+      );
+    }
+  },
+  {
+    title:'NavBar with left and right Icon',
+    render():ReactElement {
+      let leftItem = {
+        icon: require('./img/back_white.png'),
+        onPress:() => {alert('go back')},
+      };
+      let rightItem = {
+        icon: require('./img/settings_white.png'),
+        onPress:() => {alert('setting')}
+      };
+      return (
+        <MtNavBar
+          title = 'hello world'
+          leftItem={leftItem}
+          rightItem = {rightItem}
+        >
+        </MtNavBar>
+      );
+    }
+  },
+  {
+    title:'NavBar with custom element',
+    render():ReactElement {
+      let leftItem = {
+        icon: require('./img/back_white.png'),
+        onPress:() => {alert('go back')},
+      };
+      let rightItem = {
+        icon: require('./img/settings_white.png'),
+        onPress:() => {alert('setting')}
+      };
+      return (
+        <MtNavBar
+          leftItem={leftItem}
+          rightItem = {rightItem}
+        >
+          <View
+            style={ {backgroundColor:'gray',height:40,width:150}}
+          >
+          </View>
+        </MtNavBar>
+      );
+    }
+  },
 
 ];
